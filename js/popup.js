40fc850d9029ef477dd28113d6e634f0cfd84dfa
Feizerr <anastasiapred@gmail.com>
2023-03-26 18:03:28 +0300

    Добавляет валидацию формы

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,31 @@
+import { renderFullPhoto } from './full-size-photo.js';
+import { isEscapeKey } from './util.js';
+
 const bodyElement = document.querySelector('body');
 const bigPictureContainer = bodyElement.querySelector('.big-picture');
-
-const isEscapeKey = (evt) => evt.key === 'Escape';
-
-const showPopup = () => {
-  bigPictureContainer.classList.remove('hidden');
-  bodyElement.classList.add('modal-open');
-};
+const closeButton = document.querySelector('.big-picture__cancel');
 
 const closePopup = () => {
   bigPictureContainer.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
 };
 
 const onDocumentKeydown = (evt) => {
   if(isEscapeKey(evt)) {
     evt.preventDefault();
     closePopup();
-    document.removeEventListener('keydown', onDocumentKeydown);
   }
 };
 
-export { showPopup, closePopup, onDocumentKeydown };
+const showPopup = (url, likes, comments) => {
+  bigPictureContainer.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+
+  renderFullPhoto(url, likes, comments);
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+closeButton.addEventListener('click', () => closePopup());
+
+export { showPopup };
