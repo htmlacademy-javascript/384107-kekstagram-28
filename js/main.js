1506088689ab691257b6796ee69a86594f8fab21
Feizerr <anastasiapred@gmail.com>
2023-03-30 11:43:06 +0300

    Добавляет слайдер и масштабирование

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {renderThumbnails} from './thumbnails.js';
 import { createPhotos } from './data.js';
 import './form.js';
+import './scale.js';
+import './slider.js';
 
 const photos = createPhotos();
 renderThumbnails(photos);
