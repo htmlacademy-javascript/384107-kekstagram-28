f352cdc363dc0c88b0e8ba2f7c0465d097156b26
Keks <keks@htmlacademy.ru>
2023-04-06 19:56:40 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/full-size-photo.js
@@ -0,0 +1,62 @@
+const COMMENT_PORTION = 5;
+
+const fullPictureContainer = document.querySelector('.big-picture');
+const commentsList = document.querySelector('.social__comments');
+const commentTemplate = document.querySelector('#comment')
+  .content
+  .querySelector('.social__comments');
+const commentCount = fullPictureContainer.querySelector('.social__comment-count');
+const commentsLoader = fullPictureContainer.querySelector('.comments-loader');
+const shownCommentsCount = commentCount.querySelector('.comments-shown');
+const allCommentsCount = commentCount.querySelector('.comments-count');
+const image = fullPictureContainer.querySelector('img');
+const likesCount = fullPictureContainer.querySelector('.likes-count');
+
+let moduleComments = [];
+let shownComments = 0;
+
+const fillComment = ({avatar, message, name}) => {
+  const commentClone = commentTemplate.cloneNode(true);
+  const commentImage = commentClone.querySelector('.social__picture');
+  commentImage.src = avatar;
+  commentImage.alt = name;
+
+  const text = commentClone.querySelector('.social__text');
+  text.textContent = message;
+
+  return commentClone;
+};
+
+const renderComments = (comments) => {
+  commentsList.innerHTML = '';
+  commentsList.append(...comments.map(fillComment));
+};
+
+const renderFiveComments = () => {
+  shownComments += COMMENT_PORTION;
+
+  if (shownComments >= moduleComments.length) {
+    shownComments = moduleComments.length;
+    commentsLoader.classList.add('hidden');
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const comments = moduleComments.slice(0, shownComments);
+  renderComments(comments);
+
+  shownCommentsCount.textContent = shownComments;
+  allCommentsCount.textContent = moduleComments.length;
+};
+
+const renderFullPhoto = (url, likes, comments) => {
+  shownComments = 0;
+  image.src = url;
+  likesCount.textContent = likes;
+  moduleComments = comments;
+  renderFiveComments();
+};
+
+commentsLoader.addEventListener('click', renderFiveComments);
+
+export { renderFullPhoto };
---
+++ 12/js/util.js
@@ -0,0 +1,52 @@
+const getRandomInteger = (firstValue, secondValue) => {
+  const min = Math.ceil(Math.min(Math.abs(firstValue), Math.abs(secondValue)));
+  const max = Math.floor(Math.max(Math.abs(firstValue), Math.abs(secondValue)));
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const getUniqNumberFromRange = (min, max) => {
+  const previousValues = [];
+  return function() {
+    let currentValue = getRandomInteger(min, max);
+
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+
+    while(previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const createNumberGenerator = () => {
+  let firstGeneratedNumber = 0;
+
+  return function() {
+    firstGeneratedNumber += 1;
+    return firstGeneratedNumber;
+  };
+};
+
+const uniqValue = createNumberGenerator();
+
+const getRandomElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const stopEventPropagation = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.stopPropagation();
+  }
+};
+
+export {
+  getRandomInteger,
+  getUniqNumberFromRange,
+  uniqValue,
+  getRandomElement,
+  isEscapeKey,
+  stopEventPropagation,
+};
